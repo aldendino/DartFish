514816fc31389d5a7730cf2cfdf86754b56672bf
Alden Ozburn <aldendino@gmail.com>
2015-10-31 17:19:51 -0400

    Proper commenting and formatting, and variable renaming for clarity.

--- web/main.dart
+++ web/main.dart
@@ -4,16 +4,16 @@
 import 'dart:html';
 import 'dart:math';
 
-Fish fish;
-List<Fish> school;
-int mouseX = 0;
-int mouseY = 0;
-num start = 0.0;
-final int fps = 120;
-final double interval = 1000/fps;
-Random rand = new Random();
+Fish fish; // Initial fish for legacy
+List<Fish> school; // School of fish
+int mouseX = 0; // The x position of the mouse
+int mouseY = 0; // The y position of the mouse
+num lastFrame = 0.0; // Time count for animation deltas
+final int fps = 120; // Frames per second
+final double interval = 1000 / fps; // Animation interval
+Random rand = new Random(); // Fish property randomization
 
-enum Dir {Left, Right}
+enum Dir { Left, Right } // Fish direction for image choosing
 
 void main() {
   fish = new Fish(querySelector('#fish'), querySelector('#fishImg'));
@@ -29,10 +29,10 @@ void main() {
   });
 
   window.onKeyPress.listen((KeyboardEvent e) {
-    switch(e.keyCode) {
+    switch (e.keyCode) {
       case KeyCode.SPACE:
         spawnFish();
-        print(school.length + 1);
+        //print(school.length + 1);
         break;
       default:
         break;
@@ -42,21 +42,24 @@ void main() {
   window.animationFrame.then(loop);
 }
 
+/// Recursive loop for animation
 void loop(num delta) {
-  if (delta - start > interval) {
-    fish.doAnimation();
-    school.forEach((fish) => fish.doAnimation());
-    start = delta;
+  if (delta - lastFrame > interval) {
+    fish.animate();
+    school.forEach((fish) => fish.animate());
+    lastFrame = delta;
   }
   window.animationFrame.then(loop);
 }
 
+/// Spawn a new fish and add it to the school
 void spawnFish() {
   DivElement fish = new DivElement()..classes.add('abs');
   ImageElement fishImg = new ImageElement()..src = "media/fish_right.png";
   fish.children.add(fishImg);
   document.body.children.add(fish);
-  Fish egg = new Fish(fish, fishImg, rand.nextDouble() + 1);
+  Fish egg = new Fish(fish, fishImg, rand.nextDouble() + 1,
+      60 * rand.nextDouble() + 20, 40 * rand.nextDouble() + 10);
   egg.setBait(mouseX, mouseY);
   school.add(egg);
 }
@@ -68,35 +71,43 @@ class Fish {
 
   DivElement fish;
   ImageElement fishImg;
-  bool isRight = true;
-  int width;
-  int height;
-  double fishX;
-  double fishY;
-  int baitX;
-  int baitY;
-  double time;
-  double spf;
+  bool isRight; // Whether or not the fish is facing right
+  int width; // Width of the fish
+  int height; // Height of the fish
+  double fishX; // The x position the fish is centered on
+  double fishY; // The y position the fish is centered on
+  int baitX; // The x position that the fish is attracted to
+  int baitY; // The y position that the fish is attracted to
+  double time; // The time count that regulates undulation
+  double spf; // Speed factor
+  double ampX; // The horizontal undulation factor
+  double ampY; // The vertical undulation factor
 
-  Fish(DivElement fish, ImageElement fishImg, [double spf = 1]) {
+  Fish(DivElement fish, ImageElement fishImg,
+      [double spf = 1.0, double ampX = 40.0, double ampY = 30.0]) {
     this.fish = fish;
     this.fishImg = fishImg;
-    width  = 0;
+    width = 0;
     height = 0;
+    isRight = true;
     fishImg.style.src = rightImg;
     fishX = 0.0;
     fishY = 0.0;
     baitX = 0;
     baitY = 0;
-    time = 0.0;
+    time = 2 * PI * rand.nextDouble();
     this.spf = spf;
+    this.ampX = ampX;
+    this.ampY = ampY;
   }
 
+  /// Set up where the fish is heading
   void setBait(int x, int y) {
     baitX = x;
     baitY = y;
   }
 
+  /// Change the image to match the fish's direction
   void setImg(Dir dir) {
     if (dir == Dir.Left && isRight) {
       fishImg.src = leftImg;
@@ -108,8 +119,9 @@ class Fish {
     }
   }
 
-  void doAnimation() {
-    width  = fishImg.client.width;
+  /// Perform one step of animation
+  void animate() {
+    width = fishImg.client.width;
     height = fishImg.client.height;
 
     int diffX = max(fishX, baitX) - min(fishX, baitX);
@@ -131,7 +143,6 @@ class Fish {
 
     int tolX = 5;
     int tolY = 5;
-    int amp = 10;
 
     if (baitX > fishX) {
       setImg(Dir.Right);
@@ -145,26 +156,24 @@ class Fish {
       moveX = -deltaX;
     }
 
-    if (baitY > fishY + tolY + amp){// + (width / 2)) {
+    if (baitY > fishY + tolY) {
       moveY = deltaY;
-    } else if (baitY < fishY - tolY - amp){// - (width / 2)) {
+    } else if (baitY < fishY - tolY) {
       moveY = -deltaY;
     }
 
     moveX *= spf;
     moveY *= spf;
 
-    moveY += amp * sin(time);
-
     fishX += moveX;
     fishY += moveY;
 
-    int posX = (fishX - (width  / 2)).toInt();
-    int posY = (fishY - (height / 2)).toInt();
+    int posX = (fishX - (width / 2) + ampX * sin(time / 8)).toInt();
+    int posY = (fishY - (height / 2) + ampY * sin(time)).toInt();
 
     fish.style.left = '${posX}px';
-    fish.style.top  = '${posY}px';
+    fish.style.top = '${posY}px';
 
-    time += 0.1;
+    time += 0.1 % (2 * PI);
   }
 }
