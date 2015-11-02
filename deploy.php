e5efa948e79484d7e1d145a656a0b611b6ee4536
Alden Ozburn <aldendino@gmail.com>
2015-11-02 12:40:23 -0500

    Deploy script.

---
+++ deploy.php
@@ -0,0 +1,19 @@
+<?php
+$output = `git pull`;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <
+</head>
+<body>
+<div>
+    <pre>
+        <?php echo $output; ?>
+    </pre>
+</div>
+</body>
+</html>
